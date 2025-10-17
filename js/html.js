21edc89ed72eff639371ee6b3dede8180d9658f8
Elias Estrada <estradaelias162@gmail.com>
2025-10-17 15:26:39 -0300

    añadimos .js

---
+++ js/html.js
@@ -0,0 +1,33 @@
+// Definicion de una variable con let
+let nombre = "Elias";
+
+// Modificacion de una variable que ya existe
+nombre = "jose";
+
+
+const PI = 3.14;
+
+let numero = 12;
+let numeroString = "20";
+
+let booleano = true;
+
+console.log("Resultado de numero + numeroString: ", numero + numeroString);
+
+console.log("Resultado de numero + parseInt(numeroString): ", numero - parseInt(numeroString)
+);
+
+console.log("Resultado de numero - numeroString: ", numero - numeroString);
+
+/* ------------------------------------------------- */
+
+
+let entradaInput = prompt("Ingrese su nombre: ");
+
+console.log("Ingresaste: ", entradaInput);
+
+// isNaN para comprobar que no sea un numero devuelve false
+// console.log(isNaN(entradaInput));
+
+//Simil f-strings para interpolar variables
+alert(`Ingresaste: ${entradaInput} - Bienvenido!`);
